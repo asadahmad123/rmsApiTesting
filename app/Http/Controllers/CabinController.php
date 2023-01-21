a86168c07c2ce636f7c2cd234d4001bf7bf37eeb
kafeel-hub <kafeelahmed88101@gmail.com>
2023-01-21 20:29:56 +0530

    pulling code

--- app/Http/Controllers/CabinController.php
+++ app/Http/Controllers/CabinController.php
@@ -22,4 +22,19 @@ class CabinController extends Controller
 // die;
         return(dd($data));
     }
+
+    public function myfunction(){
+        $data = Cabin::all();
+        // compact('data')
+        // $cabin = new Cabin;
+        // $cabin->name = 'Room';
+        // $cabin->cabin_id = 25;
+        // $cabin->site_id = 10;
+        // $cabin->save();
+//         echo "<pre>";
+// print_r($data); 
+// echo "</pre>";
+// die;
+        return(dd($data));
+    }
 }
